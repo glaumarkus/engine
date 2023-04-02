5c88d0d72abf12830d78460306668937c2309825
Markus Glau <glaumarkus1209@gmail.com>
2023-04-02 22:40:09 +0200

    cipher and digest working

--- mb_pkcs11_engine.c
+++ mb_pkcs11_engine.c
@@ -40,11 +40,6 @@ RAND_METHOD engine_random_method = {
 
 
 
-/*
-* enum for hashing algorithms
-*/
-enum alg_type { alg_sha256 };
-
 
 /*
 * struct for digest
@@ -53,8 +48,39 @@ struct digest_ctx {
 	SHA256_CTX ctx;
 };
 
+/*
+* struct for cipher
+*/
+struct cipher_ctx {
+    EVP_CIPHER_CTX* ctx;
+    unsigned char* payload;
+};
 
+/*
+* AES 256 CBC Mapping
+*/
+static inline int engine_aes256_cbc_init(EVP_CIPHER_CTX * ctx, const unsigned char *key,
+		const unsigned char *iv, int enc);
+static inline int engine_aes256_cbc_do_cipher(EVP_CIPHER_CTX * ctx, unsigned char *out,
+		const unsigned char *in, size_t inlen);
+static inline int engine_aes256_cbc_cleanup(EVP_CIPHER_CTX *ctx);
 
+/*
+* Method mapping
+*/
+static EVP_CIPHER* engine_aes256_cbc_method = NULL;
+static const EVP_CIPHER* init_engine_aes256_cbc_method(void)
+{
+    engine_aes256_cbc_method = EVP_CIPHER_meth_new(NID_aes_256_cbc, 8, 32);
+    EVP_CIPHER_meth_set_iv_length(engine_aes256_cbc_method, 16);
+    EVP_CIPHER_meth_set_flags(engine_aes256_cbc_method, EVP_CIPH_CBC_MODE);
+    EVP_CIPHER_meth_set_init(engine_aes256_cbc_method, engine_aes256_cbc_init);
+    EVP_CIPHER_meth_set_do_cipher(engine_aes256_cbc_method, engine_aes256_cbc_do_cipher);
+    EVP_CIPHER_meth_set_cleanup(engine_aes256_cbc_method, engine_aes256_cbc_cleanup);
+    EVP_CIPHER_meth_set_impl_ctx_size(engine_aes256_cbc_method, sizeof(struct cipher_ctx));
+
+    return engine_aes256_cbc_method;
+}
 
 /*
 * SHA256 Mapping
@@ -62,7 +88,6 @@ struct digest_ctx {
 static inline int engine_sha256_init(EVP_MD_CTX *ctx);
 static inline int engine_sha256_update(EVP_MD_CTX *ctx, const void *in, size_t len);
 static inline int engine_sha256_final(EVP_MD_CTX *ctx, unsigned char *md);
-static inline int engine_sha256_copy(EVP_MD_CTX *dst, const EVP_MD_CTX *src);
 static inline int engine_sha256_cleanup(EVP_MD_CTX *ctx);
 
 /*
@@ -82,31 +107,31 @@ static const EVP_MD* init_engine_sha256_method(void)
     EVP_MD_meth_set_init(engine_sha256_method, engine_sha256_init);
     EVP_MD_meth_set_update(engine_sha256_method, engine_sha256_update);
     EVP_MD_meth_set_final(engine_sha256_method, engine_sha256_final);
-    EVP_MD_meth_set_copy(engine_sha256_method, engine_sha256_copy);
     EVP_MD_meth_set_cleanup(engine_sha256_method, engine_sha256_cleanup);
 
     return engine_sha256_method;
 }
 
 // cast to ctx
-#define CTX_CAST(ctx) ((struct digest_ctx *)(EVP_MD_CTX_md_data(ctx)))
+#define DIG_CTX_CAST(ctx) ((struct digest_ctx *)(EVP_MD_CTX_md_data(ctx)))
+
 
 static inline int engine_sha256_init(EVP_MD_CTX *ctx)
 {
     printf("engine_sha256_init called!\n");
-    return SHA256_Init(&CTX_CAST(ctx)->ctx);
+    return SHA256_Init(&DIG_CTX_CAST(ctx)->ctx);
 }
 
 static inline int engine_sha256_update(EVP_MD_CTX *ctx, const void *in, size_t len)
 {
     printf("engine_sha256_update called!\n");
-    return SHA256_Update(&CTX_CAST(ctx)->ctx, in, len);
+    return SHA256_Update(&DIG_CTX_CAST(ctx)->ctx, in, len);
 }
 
 static inline int engine_sha256_final(EVP_MD_CTX *ctx, unsigned char *md)
 {
     printf("engine_sha256_final called!\n");
-    return SHA256_Final(md, &CTX_CAST(ctx)->ctx);
+    return SHA256_Final(md, &DIG_CTX_CAST(ctx)->ctx);
 }
 
 static inline int engine_sha256_cleanup(EVP_MD_CTX *ctx)
@@ -115,13 +140,82 @@ static inline int engine_sha256_cleanup(EVP_MD_CTX *ctx)
     return 1;
 }
 
+/* AES CAST DEFINE */
+#define AES_CTX_CAST(ctx) ((struct cipher_ctx *)(EVP_CIPHER_CTX_cipher(ctx)))
 
 
+
+/* AES FUNCTIONS */
+static inline int engine_aes256_cbc_init(EVP_CIPHER_CTX * ctx, const unsigned char *key,
+		const unsigned char *iv, int enc)
+{
+    printf("engine_aes256_cbc_init called!\n");
+    // AES_CTX_CAST(ctx)->ctx = ctx;
+    // ctx = EVP_CIPHER_CTX_new();
+    // EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+    return 1;  
+}
+ 
+// will be called by both update / finalize
+static inline int engine_aes256_cbc_do_cipher(EVP_CIPHER_CTX * ctx, unsigned char *out,
+		const unsigned char *in, size_t inlen)
+{
+    printf("engine_aes256_cbc_do_cipher called!\n");
+    // int ciphertext_len = 0;
+    // EVP_EncryptUpdate(ctx, out, &ciphertext_len, in, inlen);
+    // int final_len = 0;
+    // EVP_EncryptFinal_ex(ctx, out + ciphertext_len, &final_len);
+    return 1;  
+}
+
+static inline int engine_aes256_cbc_cleanup(EVP_CIPHER_CTX *ctx)
+{
+    printf("engine_aes256_cbc_cleanup called!\n");
+    return 1;  
+}
+
+
+
+
+/*
+* Collection of Ciphers
+*/
+static int cipher_ids[] = {NID_aes_256_cbc, NID_chacha20, NID_aes_256_gcm};
+
+/*
+* Cipher selector
+*/
+static int engine_cipher_selector(ENGINE *e, const EVP_CIPHER **cipher, const int **nids, int nid)
+{
+    printf("engine_cipher_selector called!\n");
+    int ok = 1;
+
+    if (!cipher)
+    {
+        *nids = cipher_ids;
+        printf("\n Cipher is empty! Nid:%d\n", nid);
+        return 2;
+    }
+    
+    switch (nid)
+    {
+        case NID_aes_256_cbc:
+
+            printf("CipherSelector chose aes_256_cbc\n");
+            *cipher = init_engine_aes256_cbc_method();
+            break;
+
+        default:
+            *cipher = NULL;
+            ok = 0;
+    }
+    return ok;
+}
 
 /* 
 * Collection of digest ids
 */
-static int digest_ids[] = {NID_sha256};
+static int digest_ids[] = {NID_sha256, NID_sha384};
 
 /*
 * Digest selector
@@ -174,22 +268,22 @@ static int engine_finish(ENGINE* engine)
 /*
 * This will be used to get the token / id of the private key
 */
-static int engine_ctrl_cmd_string(ENGINE* e, const char* cmd_name, const char* cmd_value)
-{
-    if (strcmp(cmd_name, "CURLOPT_SSLKEY") == 0)
-    {
-        // Parse the PKCS11 URI string
-        char token_name[64], object_name[64];
-        if (sscanf(cmd_value, "pkcs11:token=%63[^;];object=%63s", token_name, object_name) != 2)
-        {
-            return 0;
-        }
-    }
+// static int engine_ctrl_cmd_string(ENGINE* e, const char* cmd_name, const char* cmd_value)
+// {
+//     if (strcmp(cmd_name, "CURLOPT_SSLKEY") == 0)
+//     {
+//         // Parse the PKCS11 URI string
+//         char token_name[64], object_name[64];
+//         if (sscanf(cmd_value, "pkcs11:token=%63[^;];object=%63s", token_name, object_name) != 2)
+//         {
+//             return 0;
+//         }
+//     }
 
-    // Handle any other options here
+//     // Handle any other options here
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 /*
@@ -200,11 +294,12 @@ int engine_bind(ENGINE * e, const char *id)
     printf("engine_bind called\n");
     if (!ENGINE_set_id(e, mb_engine_id) ||
         !ENGINE_set_name(e, mb_engine_name) ||
-        !ENGINE_set_ctrl_function(e, engine_ctrl_cmd_string) ||
+        // !ENGINE_set_ctrl_function(e, engine_ctrl_cmd_string) ||
         !ENGINE_set_init_function(e, engine_init) ||
         !ENGINE_set_finish_function(e, engine_finish) ||
         !ENGINE_set_RAND(e, &engine_random_method) ||
-        !ENGINE_set_digests(e, &engine_digest_selector)
+        !ENGINE_set_digests(e, &engine_digest_selector) ||
+        !ENGINE_set_ciphers(e, &engine_cipher_selector)
                 )
         return 0;
     return 1;
--- pkcs11_engine_test.cpp
+++ pkcs11_engine_test.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 // add this to be able to load engine
 // sudo ln -s /home/glaum/engine/build/libmbengine.so /usr/lib/x86_64-linux-gnu/engines-3/libmbengine.so
@@ -24,32 +25,45 @@ int main() {
     int init_res = ENGINE_init(mb_engine);
     printf("Engine name: %s init result : %d \n",ENGINE_get_name(mb_engine), init_res);
     
-    // Hashing
-    std::string str ("Hash this");
-    std::vector<std::uint8_t> digest;
-    digest.reserve(32);
-    unsigned int digestSize = -1;
+    int er = ENGINE_set_default_ciphers(mb_engine);
+    EVP_CIPHER_CTX* ctx;
+    ctx = EVP_CIPHER_CTX_new();
+    char text[] = "Encrypt this";
+    int clen = 0;
+    int flen = 0;
+    unsigned char* out;
+    
+    char key[] = "0000000000000000";
+    char iv[] = "0000000000000000";
+    er = EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), mb_engine, (unsigned char*)key, (unsigned char*)iv);
+    er = EVP_EncryptUpdate(ctx, out, &clen, (unsigned char*)text, sizeof(text));
+    er = EVP_EncryptFinal_ex(ctx, out + clen, &flen);
+    EVP_CIPHER_CTX_free(ctx);
 
-    int er = ENGINE_set_default_digests(mb_engine);
+    // // Hashing
+    // std::string str ("Hash this");
+    // std::vector<uint8_t> digest;
+    // digest.reserve(32);
+    // unsigned int digestSize = -1;
 
-    EVP_MD_CTX *evp_ctx;
-    evp_ctx = EVP_MD_CTX_create();
-    er = EVP_DigestInit_ex(evp_ctx, EVP_sha256(),mb_engine);
-    printf("Digest INIT %d\n",er);
+    // int er = ENGINE_set_default_digests(mb_engine);
 
-    er = EVP_DigestUpdate(evp_ctx, (unsigned char*)str.data(), str.size());
-    printf("Digest Update %d\n",er);
+    // EVP_MD_CTX *evp_ctx;
+    // evp_ctx = EVP_MD_CTX_create();
+    // er = EVP_DigestInit_ex(evp_ctx, EVP_sha256(),mb_engine);
+    // er = EVP_DigestUpdate(evp_ctx, (unsigned char*)str.data(), str.size());
+    // er = EVP_DigestFinal(evp_ctx, (unsigned char*)digest.data(), &digestSize);
+    // std::string hash_result = "1dac7205da38fc7de823435b53a5236e434fc83ab57ac69b1800ca38500256b9";
 
-    er = EVP_DigestFinal(evp_ctx, digest.data(), &digestSize);
-    printf("Digest Final %d Digest size:%d\n",er,digestSize);
+    // std::cout << "Hash1: ";
+    // for(int i= 0; i< digestSize; i++) {
+    //     printf("%x", digest[i]);
+    // }
+    // std::cout << "\n" << "Hash2: " << hash_result << "\n";
 
-    for(int i= 0; i< digestSize; i++) {
-        printf("%x", digest[i]);
-    }
-    printf("\n");
-    EVP_MD_CTX_destroy(evp_ctx);
+    // EVP_MD_CTX_destroy(evp_ctx);
     // Cleanup & free
     int fin_res = ENGINE_finish(mb_engine);
     int free = ENGINE_free(mb_engine);
     return 0;
-}
+}   
